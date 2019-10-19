d9c46d810d2e5a09233f5f54a9f9e56c92cf7695
kaloryfer10 <michael_150@wp.pl>
2019-10-19 03:07:22 +0200

    CourseManager init
    CourseManager init

--- src/screens/CourseManagerScreen/CourseManagerScreen.js
+++ src/screens/CourseManagerScreen/CourseManagerScreen.js
@@ -7,8 +7,10 @@ import {
   StyleSheet,
   ImageBackground,
   Dimensions,
+  TouchableOpacity,
   Text
 } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 
 
 const CourseManagerScreen = (props) => {
@@ -17,19 +19,33 @@ const CourseManagerScreen = (props) => {
   const {coursesList} = props.Courses;
   const course = coursesList.find(item => item.id === id);
   return (
-    <View style={courseManagerScreen.view}>
+    <View style={courseManagerStyle.view}>
       {/* <ScreenHeader title="Course Name"/> */}
-      <ImageBackground source={{uri: course.image}} style={courseManagerScreen.background}>
-        <Text>Course Name</Text>
+      <ImageBackground source={{uri: course.image}} style={courseManagerStyle.background}>
+        <View style={courseManagerStyle.userCourseStatisticsView}>
+          <Text style={courseManagerStyle.userCourseStatisticsText}>Course Name</Text>
+        </View>
       </ImageBackground>
-      <View style={courseManagerScreen.managePanelView}>
-
+      <View style={courseManagerStyle.managePanelView}>
+        <TouchableOpacity style={courseManagerStyle.portletView} activeOpacity={0.7}>
+          <Icon name="leanpub" type="font-awesome" size={30} color="#3b3b3b"/>
+          <Text style={courseManagerStyle.portletText}>Lesson</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={courseManagerStyle.portletView}>
+          <Icon name="edit" type="font-awesome" size={30} color="#3b3b3b"/>
+          <Text style={courseManagerStyle.portletText}>Test</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={courseManagerStyle.portletView}>
+          <Icon name="leanpub" type="font-awesome" size={30} color="#3b3b3b" />
+          <Text style={courseManagerStyle.portletText}>Arcade</Text>
+        </TouchableOpacity>
+        
       </View>
     </View>
   )
 }
 
-const courseManagerScreen = StyleSheet.create({
+const courseManagerStyle = StyleSheet.create({
   view: {
     flex: 1
   },
@@ -39,7 +55,6 @@ const courseManagerScreen = StyleSheet.create({
     opacity: 1,
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center',
     borderBottomWidth: StyleSheet.hairlineWidth,
     shadowColor: "#000",
     shadowOffset: {
@@ -50,9 +65,36 @@ const courseManagerScreen = StyleSheet.create({
     shadowRadius: 2.22,
     elevation: 3,
   },
+  userCourseStatisticsView: {
+    flex: 1/3,
+    width: 100 + '%',
+    backgroundColor: 'rgba(255,255,255,0.9)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  userCourseStatisticsText: {
+    fontSize: 20,
+  },
   managePanelView: {
-    flex: 3,
-
+    flex: 1/6,
+    flexDirection: 'row',
+  },
+  portletView: {
+    flex: 1/3,
+    borderColor: '#2089dc',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderStyle: 'solid',
+    borderRadius: 3,
+    padding: 5,
+    margin: 5,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  portletText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: '#3b3b3b',
   }
 
 })
